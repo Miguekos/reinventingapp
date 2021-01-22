44c6f7c69b1b95288e4e25d271138e26434c7fea
Miguel Rodriguez <miguekos1233@gmail.com>
2021-01-22 11:13:27 -0500

    upload v1.7 - 57

--- src/components/Vehiculos/IngresoVehicular.vue
+++ src/components/Vehiculos/IngresoVehicular.vue
@@ -613,6 +613,311 @@
         </q-card-actions>
       </q-form>
     </q-card>
+    <q-card v-if="tieneCodigo === 3" class="full-height" square>
+      <q-form @submit="onSubmitNew" @reset.prevent.stop="onReset">
+        <q-bar class="bg-primary text-white">
+          Ingreso Vehicular
+          <q-space />
+          <q-btn dense flat icon="close" @click="cerrar">
+            <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+        <q-card-section>
+          <!--        <TablaA />-->
+          <div class="row">
+            <div class="col-xs-12 col-md-6 q-pa-xs">
+              <q-card flat bordered class="my-card">
+                <q-markup-table dense>
+                  <thead>
+                    <tr>
+                      <td class="text-left text-h6">Datos del Vehículo</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="text-left">Fecha</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.nu_anomod }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Placa</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.co_plaveh }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Marca</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.no_marveh }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Modelo</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.no_modveh }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Año</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.nu_anofab }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Color</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.no_colveh }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Chasis</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.nu_serveh }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Motor</td>
+                      <td class="text-right">
+                        {{ dataIngresoVehicular.nu_motveh }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Kilometraje</td>
+                      <td class="text-right">
+                        <q-input filled dense v-model="val_kil" />
+                      </td>
+                    </tr>
+                  </tbody>
+                </q-markup-table>
+              </q-card>
+            </div>
+            <div class="col-xs-12 col-md-6 q-pa-xs">
+              <q-card flat bordered class="my-card">
+                <q-markup-table dense>
+                  <thead>
+                    <tr>
+                      <td class="text-left text-h6">Datos del Cliente</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="text-left">Documento de Identidad</td>
+                      <td class="text-right">
+                        <!--                        {{ clienteSelect }}-->
+                        <q-select
+                          v-if="seleccliente"
+                          filled
+                          dense
+                          @input="buscarPersonas"
+                          v-model="clienteSelect"
+                          clearable
+                          use-input
+                          hide-selected
+                          fill-input
+                          input-debounce="0"
+                          label="Cliente o Propietario"
+                          :options="options"
+                          option-label="no_person"
+                          emit-value
+                          map-options
+                          @filter="filterFn"
+                        >
+                          <template v-slot:no-option>
+                            <q-item>
+                              <q-item-section class="text-grey">
+                                No results
+                              </q-item-section>
+                            </q-item>
+                          </template>
+                          <template v-slot:append>
+                            <q-btn
+                              round
+                              dense
+                              flat
+                              icon="add"
+                              @click="seleccliente = !seleccliente"
+                            />
+                          </template>
+                        </q-select>
+                        <q-input v-else filled dense v-model="doc_ide">
+                          <template v-slot:append>
+                            <q-btn
+                              round
+                              dense
+                              flat
+                              icon="add"
+                              @click="seleccliente = !seleccliente"
+                            />
+                          </template>
+                        </q-input>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Apellido Paterno</td>
+                      <td class="text-right">
+                        <q-input filled dense v-model="ape_pat" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Apellido Materno</td>
+                      <td class="text-right">
+                        <q-input filled dense v-model="ape_mat" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Nombre del Cliente</td>
+                      <td class="text-right">
+                        <q-input filled dense v-model="nom_cli" />
+                      </td>
+                    </tr>
+                  </tbody>
+                </q-markup-table>
+              </q-card>
+            </div>
+            <div class="col-xs-12 col-md-6 q-pa-xs">
+              <q-card flat bordered>
+                <q-markup-table dense>
+                  <thead>
+                    <tr>
+                      <td class="text-left text-h6">Detalles de Ingreso</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="text-left">Centro de Operaciones</td>
+                      <td class="text-right">
+                        <q-select
+                          filled
+                          dense
+                          v-model="cen_ope"
+                          :options="optionsCentrodeOperaciones"
+                          option-label="name"
+                          option-value="value"
+                          emit-value
+                          map-options
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Dirección</td>
+                      <td class="text-right">
+                        <q-input filled dense v-model="direcci" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Descripción de ingreso</td>
+                      <td class="text-right">
+                        <q-input
+                          filled
+                          type="textarea"
+                          dense
+                          v-model="det_ing"
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </q-markup-table>
+              </q-card>
+            </div>
+            <div class="col-xs-12 col-md-6 q-pa-xs">
+              <q-card flat bordered>
+                <q-markup-table dense>
+                  <thead>
+                    <tr>
+                      <td class="text-left text-h6">Salida Próxima</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="text-left">¿Tendrá Salida Cercana?</td>
+                      <td class="text-right">
+                        <q-select
+                          filled
+                          dense
+                          v-model="swt_sal"
+                          :options="optionsTendraSalidaCercana"
+                          option-label="name"
+                          option-value="value"
+                          map-options
+                          emit-value
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="text-left">Fecha {{ fec_sal }}</td>
+                      <td class="text-right">
+                        <q-input filled dense v-model="fec_sal">
+                          <template v-slot:prepend>
+                            <q-icon name="event" class="cursor-pointer">
+                              <q-popup-proxy
+                                transition-show="scale"
+                                transition-hide="scale"
+                              >
+                                <q-date
+                                  v-model="fec_sal"
+                                  mask="YYYY-MM-DD HH:mm"
+                                >
+                                  <div class="row items-center justify-end">
+                                    <q-btn
+                                      v-close-popup
+                                      label="Close"
+                                      color="primary"
+                                      flat
+                                    />
+                                  </div>
+                                </q-date>
+                              </q-popup-proxy>
+                            </q-icon>
+                          </template>
+
+                          <template v-slot:append>
+                            <q-icon name="access_time" class="cursor-pointer">
+                              <q-popup-proxy
+                                transition-show="scale"
+                                transition-hide="scale"
+                              >
+                                <q-time
+                                  v-model="fec_sal"
+                                  mask="YYYY-MM-DD HH:mm"
+                                  format24h
+                                >
+                                  <div class="row items-center justify-end">
+                                    <q-btn
+                                      v-close-popup
+                                      label="Close"
+                                      color="primary"
+                                      flat
+                                    />
+                                  </div>
+                                </q-time>
+                              </q-popup-proxy>
+                            </q-icon>
+                          </template>
+                        </q-input>
+                      </td>
+                    </tr>
+                  </tbody>
+                </q-markup-table>
+              </q-card>
+            </div>
+          </div>
+        </q-card-section>
+
+        <!--      <q-separator />-->
+
+        <!--      <q-card-section>-->
+        <!--        {{ get_buscar_operacion.result[0] }}-->
+        <!--        <TablaBuscar :info="get_buscar_operacion.result" />-->
+        <!--        <TablaB />-->
+        <!--      </q-card-section>-->
+
+        <q-card-actions align="center">
+          <!--        <q-btn flat label="Decline" color="primary" v-close-popup />-->
+          <q-btn label="Registrar" type="submit" color="primary" />
+        </q-card-actions>
+      </q-form>
+    </q-card>
   </div>
 </template>
 
@@ -791,6 +1096,51 @@ export default {
       );
       this.$q.loading.show();
       try {
+        const responseIngresoV = await this.call_ingreso_vehicular({
+          per_reg: this.$q.localStorage.getAll().UserDetalle.co_person,
+          val_kil: this.val_kil,
+          doc_ide: this.doc_ide,
+          ape_pat: this.ape_pat,
+          ape_mat: this.ape_mat,
+          nom_cli: this.nom_cli,
+          cen_ope: this.cen_ope,
+          direcci: this.direcci,
+          det_ing: this.det_ing,
+          swt_sal: this.swt_sal,
+          fec_sal: this.fec_sal
+        });
+        if (responseIngresoV.res === "ok") {
+          this.$q.notify({
+            message: "Creando"
+          });
+          this.$store.commit("example/dialogIngresoVehicular", false);
+          await this.$router.push("/operaciones?id=1");
+          this.$q.loading.hide();
+        } else if (responseIngresoV.res === "ko") {
+          this.$q.notify({
+            message: "Creando"
+          });
+          this.$q.loading.hide();
+        } else {
+          this.$q.notify({
+            message: "Creando"
+          });
+          this.$q.loading.hide();
+        }
+      } catch (e) {
+        console.log(e);
+        this.$q.loading.hide();
+      }
+    },
+    async onSubmitNew() {
+      console.log("onSubmit", this.dataIngresoVehicular);
+      console.log(this.$q.localStorage);
+      console.log(
+        "Codigo de persona",
+        this.$q.localStorage.getAll().UserDetalle.co_person
+      );
+      this.$q.loading.show();
+      try {
         const responseIngresoV = await this.call_ingresar_vehicu({
           per_reg: this.$q.localStorage.getAll().UserDetalle.co_person,
           pla_veh: this.dataIngresoVehicular.co_plaveh,
@@ -883,7 +1233,7 @@ export default {
           ...this.dataIngresoVehicular,
           ...this.getVehiculosFilter[0]
         });
-        this.tieneCodigo = 1;
+        this.tieneCodigo = 3;
       } else {
         this.tieneCodigo = 2;
       }
